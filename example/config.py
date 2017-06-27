798f070259fe60271bfc6724427d461b16da01ba
antikytheraton <binauralvoice@gmail.com>
2017-06-27 13:56:28 -0500

    Init

---
+++ example/config.py
@@ -0,0 +1,5 @@
+CONFIG = {
+    'FACEBOOK_TOKEN': 'EAANgzn2wUtoBADwVamrsDaGZBRrFiEKz7UXtpALkmiH1WNzBPZCtbvbSsDGGZAbmBbdJZChg7ivMMdURQAwiRCsoiB41LDC8haMUzSutGti88ebZBx5smzLpR0xMzjlsfetrrCdWdcjTq8Onhr0bbZB11l67YKuV0OpQpalmFgLQZDZD',
+    'VERIFY_TOKEN': 'verify_simsimbot',
+    'SERVER_URL': 'https://karl.ohrocks.com'
+}
