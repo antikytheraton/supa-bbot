152f9df3b020a7518b7fb917c311c41bfd270e64
antikytheraton <binauralvoice@gmail.com>
2017-06-27 14:10:37 -0500

    config

--- example/config.py
+++ example/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
     'FACEBOOK_TOKEN': 'EAANgzn2wUtoBADwVamrsDaGZBRrFiEKz7UXtpALkmiH1WNzBPZCtbvbSsDGGZAbmBbdJZChg7ivMMdURQAwiRCsoiB41LDC8haMUzSutGti88ebZBx5smzLpR0xMzjlsfetrrCdWdcjTq8Onhr0bbZB11l67YKuV0OpQpalmFgLQZDZD',
-    'VERIFY_TOKEN': 'verify_simsimbot',
-    'SERVER_URL': 'https://karl.ohrocks.com'
+    'VERIFY_TOKEN': 'verify_supabbot',
+    'SERVER_URL': 'https://super-boletos-bot.herokuapp.com/'
 }
