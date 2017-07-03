481cef6be2038e39ebf0197620a2d2147d5d4e30
antikytheraton <binauralvoice@gmail.com>
2017-07-03 11:48:35 -0500

    static

--- example/config.py
+++ example/config.py
@@ -12,5 +12,5 @@
 CONFIG = {
     'FACEBOOK_TOKEN': 'EAANdhKleoKMBAKMRAe7x5nq46724O1HtEx5u39DoSdzoGTgfi0qYEQCq7VsDmZAu2CDKDe7UgZAValHgu4ZBYM4MrgJQL3yOl5Moe56mDbTRbu2Y6I7ROfjSKsZCpN9ybrrugsi0bqZCqL77iC6BzTgzAIfKZBhjGMknj80uZB6ZBzhE6mWzUka5',
     'VERIFY_TOKEN': 'verify_supabbot',
-    'SERVER_URL': 'https://super-boletos-bot.herokuapp.com'
+    'SERVER_URL': 'https://0d6471e7.ngrok.io'
 }
